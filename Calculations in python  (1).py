ae1b39a6cc9998d7317f1aa3f63c4e920f0f1d0f
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-08-21 14:09:30 +0500

    Add files via upload

---
+++ Calculations in python  (1).py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Sum is :",4+5)
+
+
+# In[3]:
+
+
+print("subtraction cause 5 and 4 to change into ",5-4)
+
+
+# In[6]:
+
+
+print("Multiply 2 and 3 and get ",2*3)
+
+
+# In[7]:
+
+
+print("divide 10 by 5 results ",10/5)
+
+
+# In[8]:
+
+
+print("Module of 5 and 2 is ",5%2)
+
+
+# In[12]:
+
+
+# calculation of sum of two numbers taken from the user:
+number1=int(input("Enter the number 1 :"))
+number2=int(input("Enter the number 2 :"))
+sum=number1+number2
+average=sum/2
+square_root=sum**0.5
+print("The sum if two numbers is :",sum)
+print("The average of two numbers is :",average)
+print("The square root of sum of two numbers is :",square_root)
+
+
+# In[15]:
+
+
+#calculation of area of triangle rectangle and circle:
+length=int(input("Enter the length :"))
+width=int(input("Enter the width :"))
+area_of_triangle=1/2*(length*width)
+print("The area of triangle is :",area_of_triangle)
+#calculate area of rectangle:
+area_of_rectangle=length*width
+print("Area of  rectangle ",area_of_rectangle)
+#calculation of area of circle:
+pie=3.14
+radius_of_circle=int(input("Enter the radius of circle :"))
+area_of_circle=pie*(radius_of_circle**2)
+print("The area of circle is :",area_of_circle)
+
+
+# In[ ]:
+
+
+
+
